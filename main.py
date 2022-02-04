968c1d38a83bf473523299d55981be9f7ba5e17b
fotto <felix.otto@lebihan.de>
2022-02-04 13:34:09 +0100

    autoupdate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from msilib.schema import ComboBox
-import os,sys
-from PyQt5.QtWidgets import (
-    QApplication, QDialog, QMainWindow, QMessageBox
-)
 from PyQt5.uic import loadUi
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication, QDialog, QMessageBox
 from gui.minertools_gui_main_window import Ui_MainWindow
 from tkinter import messagebox
 from numpy import genfromtxt, loadtxt
@@ -14,15 +9,19 @@ import src.ssh_comms
 from zipfile import ZipFile
 import sys
 import subprocess
-
+import psutil
+import time
 #*************************** BUTTON FUNCTIONS ***************************
 class Window(QMainWindow, Ui_MainWindow):
+    """A dummy docstring."""
     def __init__(self, parent=None):
+        """A dummy docstring."""
         super().__init__(parent)
         self.setWindowIcon(QtGui.QIcon('assets/helium.ico'))
         self.setupUi(self)
             
 def main():
+    """A dummy docstring."""
     app = QApplication(sys.argv)
     win = Window()
     win.show()
--- minertools-update.py
+++ minertools-update.py
@@ -3,9 +3,30 @@ import sys
 import subprocess
 from asyncio import subprocess
 from subprocess import Popen
+import psutil
+import pathlib
+import time
 
+def wait_until():
+    while True:
+        if ("MinerTools.exe" in (p.name() for p in psutil.process_iter())): 
+            time.sleep(5)
+            print("läuft")
+        else:
+            print("läuft nicht")
+            return False
+
+updatepath = pathlib.Path().resolve()
+print(updatepath)
+f = open("startupdate.txt","w+")
+f.write(str(updatepath))
+f.close
+wait_until()
+print("unzipping .......................")
 with ZipFile('miner-update.zip', 'r') as zipOjk:
     zipOjk.extractall()
-
-Popen('MinerTools.exe')
+f = open("endupdate.txt","w+")
+f.write("geupdated")
+f.close
+#Popen('%s\MinerTools.exe' % updatepath)
 sys.exit()
